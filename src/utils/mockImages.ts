d5ba426d9eb79cb2265e155ef469c023fdc8e40b
javalvarenga <abraalvare@gmail.com>
2024-10-15 20:55:43 -0600

    initial changes

--- src/utils/mockImages.ts
+++ src/utils/mockImages.ts
@@ -1,4 +1,5 @@
 const baseUrl = 'https://image.xiaomo.info/mock';
-export const mockImgCover = (index: number): string => `${baseUrl}/cover/cover_${index}.jpg`;
-export const mockImgProduct = (index: number): string => `${baseUrl}/product/product_${index}.jpg`;
-export const mockImgAvatar = (index: number): string => `${baseUrl}/avatar/avatar_${index}.jpg`;
+const img = 'https://meatpack.com/cdn/shop/files/DD1391-100_31b3fdf4-b4fb-4f99-84af-a41dc19dcc0e_576x576.jpg?v=1702568903'
+export const mockImgCover = (index: number): string => `${img}`;
+export const mockImgProduct = (index: number): string => `${img}`;
+export const mockImgAvatar = (index: number): string => `${img}`;
