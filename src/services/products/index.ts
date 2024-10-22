e26e6816fb2d81cb9bdb4cb4b214165bde443c2f
javalvarenga <abraalvare@gmail.com>
2024-10-22 11:33:30 -0600

    hook and services

---
+++ src/services/products/index.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const PROD_URL = 'http://localhost:3001';
+const BASE_URL = 'api/products';
+
+export const getAllProducts = async () => {
+    /*     const serviceToken = window.localStorage.getItem('serviceToken');
+     */
+    const url = `${PROD_URL}/${BASE_URL}/getProducts`;
+
+    try {
+        const response = await axios.get(url);
+
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching billing orders:', error);
+        throw error;
+    }
+};
+
+
+
+export const getProduct = async (productId) => {
+    /*     const serviceToken = window.localStorage.getItem('serviceToken');
+     */
+    const url = `${PROD_URL}/${BASE_URL}/getProducts`;
+
+    try {
+        const response = await axios.get(url, {
+            /* headers: {
+            Authorization: `${serviceToken}`
+            }, */
+            params: {
+                productId: productId,
+            }
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching billing orders:', error);
+        throw error;
+    }
+};
